1b3bc3741cd28d6bd70c61dfe955c2a7a9535651
TaVa <mihail.zvezdinskiy@gmail.com>
2020-03-20 17:28:23 +0200

    refactor task3 correct

--- task3.js
+++ task3.js
@@ -2,22 +2,25 @@ function sum (a, b, c, d) {
     return a + b + c + d;
 }
 function curry(sum) {
-    let arr = [];
     return  function hz() {
+        let arr = [];
         arr = arr.concat([].slice.call(arguments));
         if (arr.length < 4) {
-            return hz;
+            return function hz1() {
+                arr = arr.concat([].slice.call(arguments));
+                if (arr.length < 4) {
+                    return hz1;
+                } else {
+                    return sum(...arr);
+                }
+            };
         } else {
-            let temp = arr;
-            arr = [];
-            return sum(...temp);
+            return sum(...arr);
         }
     }
 }
 const f = curry(sum);
-console.log(f(1,2));
-// console.log(f(1,2)(3,4));
-// console.log(f(1)(2)(3)(4));
-// console.log(f(1)(2,3,4));
-// console.log(f(5,6,7,8));
-console.log(f(3,4));
+console.log(f(1,2)(3,4));
+console.log(f(1)(2)(3)(4));
+console.log(f(1)(2,3,4));
+console.log(f(5,6,7,8));
